d21236b249ab10a0acc62a32547bfbc061012e99
mister <mister@DESKTOP-534BRGJ>
2018-04-14 22:15:17 +0530

    test on Test1.java

--- gitTest/src/gitTest/Test1.java
+++ gitTest/src/gitTest/Test1.java
@@ -3,6 +3,6 @@ package gitTest;
 public class Test1 {
 public static void main(String[] args) {
 	System.out.println("tset1 is passed");
-	System.out.println("Test1 out put changed");
+	System.out.println("Test1 output changed");
 }
 }
